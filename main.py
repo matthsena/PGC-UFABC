872201dc9abfe4af16c622b98054bfca227a96c9
Matheus <matheusalexandre.sena99@gmail.com>
2023-05-30 11:11:12 -0300

    refactor: return data in list

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import scrapy
+import requests
 
-class TopViewsSpider(scrapy.Spider):
-    name = "topviews"
-    start_urls = [
-        "https://pageviews.wmcloud.org/topviews/?project=en.wikipedia.org&platform=all-access&date=2022&excludes="
-    ]
+# URL of the Wikipedia API
+url = 'https://pt.wikipedia.org/w/api.php'
 
-    def parse(self, response):
-        for tr in response.css("tbody.topview-entries tr.topview-entry"):
-            yield {
-                "rank": tr.css("td.rank::text").get(),
-                "title": tr.css("td.title a::text").get(),
-                "views": tr.css("td.views::text").get(),
-                "link": tr.css("td.topview-entry--label a::href").get()
-            }
+# Parameters for the API request
+params = {
+    'action': 'query',
+    'prop': 'extracts',
+    'titles': 'The_Boys_(TV_series)',
+    'format': 'json',
+    'explaintext': True,
+    'exsectionformat': 'wiki',
+    'exsentences': 5
+}
+
+# Send the API request
+response = requests.get(url, params=params)
+
+# Get the page content from the API response
+page = next(iter(response.json()['query'].values()))
+
+# Print the page content
+print(response.json)
