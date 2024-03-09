900aa448de02a32ff9d1dc2e93cf844e539f3f38
Matheus Sena <matheusalexandre.sena99@gmail.com>
2024-03-09 18:11:32 -0300

    refactor: modularizando vgg

--- vgg_run.py
+++ vgg_run.py
@@ -1,33 +1,29 @@
 from keras.applications.vgg19 import VGG19, preprocess_input
-import keras.utils as image
 from scipy.spatial import distance
-import numpy as np
 import os
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 import time
-from scores import simple_score, decay_score, diversity_score, new_score
-import sys
+from scores import new_score
+from itertools import combinations, product
+from models.cv.vgg19 import FeatureExtractor
+
+vgg19 = FeatureExtractor()
+
 
 start_time = time.time()
 
-model = None
-
 # read all folders names in data folder
-data_dir = 'data-cidades'
+data_dir = 'data-quentes'
 
 folders = [f for f in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, f))]
 
 final_df = pd.DataFrame()
 
+vgg19 = FeatureExtractor()
+
 for folder in folders:
-    base_folder = f'data-cidades/{folder}/'
-
-
-    def load_model():
-        global model
-        if model is None:
-            model = VGG19(weights='imagenet', include_top=False)
+    base_folder = f'data-quentes/{folder}/'
 
 
     def get_img_files(base_path):
@@ -35,16 +31,6 @@ for folder in folders:
         file_list = [file_name for file_name in os.listdir(
             folder_path) if os.path.isfile(os.path.join(folder_path, file_name))]
         return (base_path, sorted(file_list))
-
-
-    def extract_features_vgg19(img_path):
-        img = image.load_img(os.path.join(
-            base_folder, img_path), target_size=(224, 224))
-        x = image.img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
-        features = model.predict(x)
-        return features.flatten()
 
 
     def compare_features(features1, features2):
@@ -55,49 +41,42 @@ for folder in folders:
 
     img_lang_files = list(map(get_img_files, langs_to_check))
 
-    load_model()
-
     # Extract features for all images first
     features_dict = {}
     for lang, photos in img_lang_files:
         for photo in photos:
             img_path = os.path.join(lang, photo)
-            features_dict[img_path] = extract_features_vgg19(img_path)
+            img = os.path.join(base_folder, img_path)
+            features_dict[img_path] = vgg19.extract_features(img)
 
     list_to_compare = []
     seen = set()
 
-    for lang, photos in img_lang_files:
-        for other_lang, other_photos in img_lang_files:
-            if lang != other_lang:
-                for photo in photos:
-                    for other_photo in other_photos:
-                        sorted_items = tuple(
-                            sorted([lang, other_lang, photo, other_photo]))
-                        if sorted_items not in seen:
-                            seen.add(sorted_items)
-                            list_to_compare.append(
-                                ((lang, other_lang), (photo, other_photo)))
+    for (lang_1, imgs_1), (lang_2, imgs_2) in combinations(img_lang_files, 2):
+        for img_1, img_2 in product(imgs_1, imgs_2):
+            items = tuple(sorted([lang_1, lang_2, img_1, img_2]))
+            if items not in seen:
+                seen.add(items)
+                list_to_compare.append(((lang_1, lang_2), (img_1, img_2)))
 
     result_list = []
 
     with ThreadPoolExecutor() as executor:
         futures = []
-        for (lang1, lang2), (img1, img2) in list_to_compare:
+        for (lang_1, lang_2), (img1, img2) in list_to_compare:
             future = executor.submit(compare_features, features_dict[os.path.join(
-                lang1, img1)], features_dict[os.path.join(lang2, img2)])
-            futures.append((future, (lang1, lang2, img1, img2)))
+                lang_1, img1)], features_dict[os.path.join(lang_2, img2)])
+            futures.append((future, (lang_1, lang_2, img1, img2)))
 
-        for future, (lang1, lang2, img1, img2) in futures:
+        for future, (lang_1, lang_2, img1, img2) in futures:
             result_list.append({
                 'article': folder,
-                'original': lang1,
-                'compare': lang2,
+                'original': lang_1,
+                'compare': lang_2,
                 'original_photo': img1,
                 'compare_photo': img2,
                 'distance': future.result()
             })
-            # print(f'{lang1}/{img1} -> {lang2}/{img2}: {future.result()}')
 
     df_result = pd.DataFrame(result_list)
 
